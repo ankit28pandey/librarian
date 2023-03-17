09dcd0a522a597401d8fb9b5ecc572d470b9c77a
ankit28pandey <ankit28.ap@gmail.com>
2023-03-17 14:51:06 +0530

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="center">
+        <v-row>
+            <v-col cols="10" offset="1">
+                <v-avatar color="primary" size="95" tile>
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+                <v-text-field label="Username"></v-text-field>
+                <v-text-field label="Password" type="password"></v-text-field>
+                <v-btn block @click="authenticate()">
+                    Login
+                  </v-btn>
+            </v-col>
+        </v-row>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: "",
+                password: "",
+            }
+        },
+
+        methods: {
+            authenticate() {
+                alert("Login Successful")
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
