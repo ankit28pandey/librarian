d84a533a36f47129baf5809d593357245f2949f7
ankit28pandey <ankit28.ap@gmail.com>
2023-03-20 15:03:03 +0530

    first commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="center">
         <v-row>
-            <v-col cols="10" offset="1">
+            <v-col cols="10" offset="1" sm="6" offset-sm="3" md="4" offset-md="4">
                 <v-avatar color="primary" size="95" tile>
                     <v-img src="@/assets/logo.png"></v-img>
                 </v-avatar>
